5b84a4769c5ee536a1bc5d9a36fd9486319e59bc
Christopher Murray <cmurray1105@gmail.com>
2020-12-15 14:02:20 -0600

    completed aVeryBigSum

---
+++ aVeryBigSum/aVeryBigSum.js
@@ -0,0 +1,9 @@
+// Input:  an array of integers
+// Output: the sum of all array elements
+function aVeryBigSum(ar) {
+let sum = 0;
+for (let i = 0; i < ar.length; i++){
+  sum += ar[i]
+}
+return sum
+}
