142a41fed0535286eed0451c83af455946bf04e9
Christopher Murray <cmurray1105@gmail.com>
2020-12-15 14:42:22 -0600

    commit

--- plusMinus/plusMinus.js
+++ plusMinus/plusMinus.js
@@ -1,21 +1,25 @@
 // Input: an array of integers
-// Output: an array of the ratio of positive, negative, and zeroes within the array.
+// Output: console log the ratio of positives zeroes and negatives
 
 function plusMinus(arr) {
-// create variables for all the sums
-let positiveCount = 0;
-let negativeCount = 0;
-let zeroCount = 0;
-// iterate through arr
-for (let i = 0; i < arr.length; i++){
-  // determine if the number is positive, negative, or zero and increment the appropriate counter
-  if(arr[i] > 0){
-    positiveCount++
-  } else if(arr[i] === 0) {
-    zeroCount++
-  } else {
-    negativeCount++
+  // create variables for all the sums
+  let positiveCount = 0;
+  let negativeCount = 0;
+  let zeroCount = 0;
+  // iterate through arr
+  for (let i = 0; i < arr.length; i++) {
+    // determine if the number is positive, negative, or zero and increment the appropriate counter
+    if (arr[i] > 0) {
+      positiveCount++;
+    } else if (arr[i] === 0) {
+      zeroCount++;
+    } else {
+      negativeCount++;
+    }
   }
+  // return an array with the averages
+
+    console.log(positiveCount / arr.length)
+    console.log(negativeCount / arr.length)
+    console.log(zeroCount / arr.length)
 }
-// return an array with the averages
-return ([positiveCount/arr.length,negativeCount/arr.length, zeroCount/arr.length])
